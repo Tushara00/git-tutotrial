a350e9ebeb3b52d22be8c1cb049f146fefec5f89
trisha <trishaahamed300@gmail.com>
2021-12-10 20:51:56 +0600

    edir

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 public class hello {
     public static void main(String[] args){
         System.out.println("VS code is awesome");
-        greeting("Halima");
+        greeting("git");
     }
     public static void greetings(String){
-        System.out.println("Hello" + name);
+        System.out.println("bye bye" + name);
     }
 }
