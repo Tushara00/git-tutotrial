c9bd0b5d040fded70cb00c66ae905ad4cf0c12d1
trisha <trishaahamed300@gmail.com>
2021-12-10 20:01:40 +0600

    new add

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class hello {
     public static void main(String[] args){
         System.out.println("VS code is awesome");
+        greeting("Halima");
+    }
+    public static void greetings(String){
+        System.out.println("Hello" + name);
     }
 }
