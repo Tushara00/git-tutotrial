7778aad44f8efce216c9ab951a89dac8c5cca1d0
trisha <trishaahamed300@gmail.com>
2021-12-10 21:29:26 +0600

    master branch

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class hello {
     public static void main(String[] args){
         System.out.println("VS code is awesome");
-        greeting("Halima");
+        greeting("Halima master branch");
     }
     public static void greetings(String){
         System.out.println("Hello" + name);
