7edfc88c2747927894b77bf47468521da66b8102
trisha <trishaahamed300@gmail.com>
2021-12-10 21:43:36 +0600

    branche b

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class hello {
     public static void main(String[] args){
         System.out.println("VS code is awesome");
-        greeting("git");
+        greeting("git brach b");
     }
     public static void greetings(String){
         System.out.println("bye bye" + name);
