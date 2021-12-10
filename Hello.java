48a4c64d62d7ae4ff159f76744549cc9b73fbd83
trisha <trishaahamed300@gmail.com>
2021-12-10 21:32:26 +0600

    branch a

--- Hello.java
+++ Hello.java
@@ -1,13 +1,16 @@
 public class hello {
     public static void main(String[] args){
         System.out.println("VS code is awesome");
-        greeting("git");
+        greeting("git branch b");
     }
     public static void greetings(String){
         System.out.println("bye bye" + name);
     }
 
     public static int sqr (int x){
+        return x*x;
+    }
+    public static int brtancha (int x){
         return x*x;
     }
 }
